5eb13d92c2318cd4cf89614d8d1ec381a53960d4
AugustinSorel <sorelaugustin@gmail.com>
2023-05-24 18:54:20 +0200

    auth provider added

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,11 +2,14 @@
 
 import { ThemeProvider } from "next-themes";
 import type { PropsWithChildren } from "react";
+import { SessionProvider } from "next-auth/react";
 
 export const Providers = ({ children }: PropsWithChildren) => {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      {children}
-    </ThemeProvider>
+    <SessionProvider>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        {children}
+      </ThemeProvider>
+    </SessionProvider>
   );
 };
