c5b76b7d463f5f9d022b1b4e889b0bddca273b06
AugustinSorel <sorelaugustin@gmail.com>
2023-05-25 18:47:10 +0200

    github provdier added

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,12 +2,23 @@ import { env } from "@/env.mjs";
 import { getServerSession } from "next-auth";
 import type { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
+
+console.log({
+  clientId: env.GITHUB_CLIENT_ID,
+  clientSecret: env.GITHUB_CLIENT_SECRET,
+  allowDangerousEmailAccountLinking: true,
+});
 
 export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
       clientId: env.GOOGLE_CLIENT_ID,
       clientSecret: env.GOOGLE_CLIENT_SECRET,
+    }),
+    GitHubProvider({
+      clientId: env.GITHUB_CLIENT_ID,
+      clientSecret: env.GITHUB_CLIENT_SECRET,
     }),
   ],
   pages: {
