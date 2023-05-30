ae4de24af6af2b579c2593d5b1a6e2f0f1259216
AugustinSorel <sorelaugustin@gmail.com>
2023-05-30 14:43:14 +0200

    drop down added

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,15 +2,17 @@ import Link from "next/link";
 import { NewExerciseNameForm } from "./newExerciseNameForm";
 import { newExerciseNameSchema } from "@/schemas/exerciseSchemas";
 import { Skeleton } from "@/components/ui/skeleton";
+import { Button } from "@/components/ui/button";
+import { Edit2, MoreHorizontal, Trash } from "lucide-react";
 import {
-  ContextMenu,
-  ContextMenuContent,
-  ContextMenuItem,
-  ContextMenuLabel,
-  ContextMenuSeparator,
-  ContextMenuTrigger,
-} from "@/components/ui/context-menu";
-import { Edit2, Trash } from "lucide-react";
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 const DashboardPage = () => {
   const action = async (formData: FormData) => {
@@ -34,32 +36,47 @@ const DashboardPage = () => {
 
       <ul className="mx-auto grid max-w-[calc(var(--exercise-card-height)*4+20px*3)] grid-cols-[repeat(auto-fill,minmax(min(100%,var(--exercise-card-height)),1fr))] gap-5">
         {[...Array<unknown>(10)].map((_, i) => (
-          <ContextMenu key={i}>
-            <ContextMenuTrigger asChild>
-              <li className="relative h-exercise-card rounded-md border border-border bg-primary backdrop-blur-md duration-300 hover:scale-[1.02] hover:bg-border">
-                <header className="border-b border-border bg-primary p-2">
-                  <p className="truncate capitalize">bench press</p>
-                </header>
+          <div
+            className="group relative h-exercise-card rounded-md border border-border bg-primary backdrop-blur-md duration-300 hover:scale-[1.02] hover:bg-border"
+            key={i}
+          >
+            <header className="flex items-center justify-between border-b border-border bg-primary p-2">
+              <Link
+                href="/exercises/1"
+                className="absolute inset-0 -z-10 duration-300"
+              />
 
-                <Link
-                  href="/exercises/1"
-                  className="absolute inset-0 -z-10 duration-300"
-                />
-              </li>
-            </ContextMenuTrigger>
-            <ContextMenuContent className="w-64">
-              <ContextMenuLabel>Settings</ContextMenuLabel>
-              <ContextMenuSeparator />
-              <ContextMenuItem>
-                <Edit2 className="mr-2 h-4 w-4" />
-                <span className="capitalize">rename</span>
-              </ContextMenuItem>
-              <ContextMenuItem className="text-destructive/80 focus:bg-destructive/20 focus:text-destructive">
-                <Trash className="mr-2 h-4 w-4" />
-                <span className="capitalize">delete exercise</span>
-              </ContextMenuItem>
-            </ContextMenuContent>
-          </ContextMenu>
+              <p className="truncate capitalize">bench press</p>
+
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button
+                    className="h-8 p-1 opacity-0 transition-all duration-100 group-focus-within:opacity-100 group-hover:opacity-100 aria-[expanded=true]:opacity-100"
+                    size="icon"
+                    variant="ghost"
+                  >
+                    <MoreHorizontal className="h-4 w-4" />
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-56">
+                  <DropdownMenuLabel className="capitalize">
+                    settings
+                  </DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuGroup>
+                    <DropdownMenuItem>
+                      <Edit2 className="mr-2 h-4 w-4" />
+                      <span className="capitalize">rename</span>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem className="text-destructive/80 focus:bg-destructive/20 focus:text-destructive">
+                      <Trash className="mr-2 h-4 w-4" />
+                      <span className="capitalize">delete exercise</span>
+                    </DropdownMenuItem>
+                  </DropdownMenuGroup>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </header>
+          </div>
         ))}
 
         {[...Array<unknown>(10)].map((_, i) => (
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -16,10 +16,11 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-input hover:bg-accent hover:text-accent-foreground",
+          "border border-border hover:bg-primary hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
+        ghost:
+          "hover:bg-primary border-border hover:border hover:text-accent-foreground",
         link: "underline-offset-4 hover:underline text-primary",
       },
       size: {
