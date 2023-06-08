12dc9a9ce7ae9907c1c8a0730b1d6e8ec4072a88
AugustinSorel <sorelaugustin@gmail.com>
2023-06-08 17:27:38 +0200

    form renamed

--- src/app/exercises/[id]/layout.tsx
+++ src/app/exercises/[id]/layout.tsx
@@ -2,13 +2,13 @@ import { DashboardBackground } from "@/components/ui/dashboardBackground";
 import type { ReactNode } from "react";
 
 type Props = {
-  newDataForm: ReactNode;
+  newExerciseDataForm: ReactNode;
 };
 
 const Layout = (props: Props) => {
   return (
     <main className="relative min-h-[calc(100vh-var(--header-height))]">
-      <div className="space-y-5 p-5">{props.newDataForm}</div>
+      <div className="space-y-5 p-5">{props.newExerciseDataForm}</div>
 
       <div className="absolute inset-0 -top-[var(--header-height)] -z-10">
         <DashboardBackground />
