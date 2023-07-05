3cb31b26bd1cecde04d045799ae4da65ae93139a
AugustinSorel <sorelaugustin@gmail.com>
2023-07-05 17:52:25 +0200

    card link fixed

--- src/app/dashboard/@exerciseCard/page.tsx
+++ src/app/dashboard/@exerciseCard/page.tsx
@@ -21,11 +21,11 @@ const ExerciseCard = () => {
     <li className="group relative flex h-exercise-card flex-col overflow-hidden rounded-md border border-border bg-primary backdrop-blur-md duration-300 hover:scale-[1.02] hover:bg-border">
       <Link
         href="/exercises/bench-press?timeframe=1m"
-        className="absolute inset-0 -z-10 duration-300"
+        className="absolute inset-0 duration-300"
         aria-label={`go to exercise ${exerciseName}`}
       />
 
-      <header className="flex items-center justify-between gap-2 border-b border-border bg-primary p-2">
+      <header className="z-10 flex items-center justify-between gap-2 border-b border-border bg-primary p-2">
         <p className="truncate capitalize">{exerciseName}</p>
 
         <CardDropDown />
