ab53c6bfd50d57ceaf80df9d9c3f0108add51ad3
AugustinSorel <sorelaugustin@gmail.com>
2023-07-07 09:54:42 +0200

    not found page added

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,17 @@
+import { Button } from "@/components/ui/button";
+import { FileWarning } from "lucide-react";
+import Link from "next/link";
+
 const NotFound = () => {
   return (
-    <div>
-      <h1>404</h1>
-      <p>Page not found</p>
-    </div>
+    <main className="absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 flex-col items-center space-y-5 text-center">
+      <FileWarning className="h-24 w-24 stroke-muted-foreground" />
+      <h1 className="text-6xl font-bold uppercase">404 error</h1>
+      <p className="text-xl text-muted-foreground">Sorry page not found</p>
+      <Button asChild size="lg">
+        <Link href="/">home</Link>
+      </Button>
+    </main>
   );
 };
 
