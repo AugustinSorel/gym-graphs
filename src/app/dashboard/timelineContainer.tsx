4bf39f1195864dd673de82f793a0b569847acfd1
AugustinSorel <sorelaugustin@gmail.com>
2023-08-01 21:21:56 +0200

    FEATURE/timeline component created

---
+++ src/app/dashboard/timelineContainer.tsx
@@ -0,0 +1,10 @@
+import type { ComponentProps } from "react";
+
+export const TimelineContainer = (props: ComponentProps<"div">) => {
+  return (
+    <div
+      {...props}
+      className="relative flex flex-col gap-5 pb-20 text-center before:absolute before:-left-10 before:bottom-0 before:top-0 before:w-1 before:bg-border after:absolute after:-left-12 after:top-0 after:aspect-square after:w-5 after:rounded-full after:bg-border after:backdrop-blur-xl last:pb-0"
+    />
+  );
+};
