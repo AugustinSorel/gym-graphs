421080da1b9df7fa2f6450371504fe1a097bc7f5
AugustinSorel <sorelaugustin@gmail.com>
2023-08-10 16:27:29 +0200

    FEATURE/drizzle adapter installed

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,5 +3,5 @@ import type { Config } from "drizzle-kit";
 export default {
   schema: "./src/db/schema.ts",
   connectionString: process.env.DB_URL,
-  out: "./migration-folder",
+  out: "./drizzle",
 } satisfies Config;
