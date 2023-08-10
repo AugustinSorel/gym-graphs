2f25cbbe6efefd21c6e12efb6b034ab26f57e9ce
AugustinSorel <sorelaugustin@gmail.com>
2023-08-10 17:23:09 +0200

    FIX/middleware updated

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,19 @@
+import { env } from "./env.mjs";
+import { withAuth } from "next-auth/middleware";
+
 //TODO: hide home and singin page?
-export { default } from "next-auth/middleware";
+
+export default withAuth({
+  secret: env.NEXTAUTH_SECRET,
+
+  callbacks: {
+    authorized: ({ req, token }) => {
+      if (token) {
+        return true;
+      }
+      return false;
+    },
+  },
+});
 
 export const config = { matcher: ["/dashboard/:path*", "/exercises/:path*"] };
