9dd7f9cdc8c96921bd972a54a425758701fb5e5f
AugustinSorel <sorelaugustin@gmail.com>
2023-08-11 11:53:42 +0200

    REFACTOR/withAuth removed

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,5 @@
-import { env } from "./env.mjs";
-import { withAuth } from "next-auth/middleware";
-
 //TODO: hide home and singin page?
 
-export default withAuth({
-  secret: env.NEXTAUTH_SECRET,
-});
+export { default } from "next-auth/middleware";
 
 export const config = { matcher: ["/dashboard/:path*", "/exercises/:path*"] };
