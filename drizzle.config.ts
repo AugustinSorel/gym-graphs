234f6130dedd49528596aef1545e7aba5813f102
AugustinSorel <sorelaugustin@gmail.com>
2023-08-12 15:32:50 +0200

    FEATURE/user schema added

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,9 @@
-import { env } from "@/env.mjs";
 import type { Config } from "drizzle-kit";
 
 export default {
   schema: "./src/db/schema.ts",
-  dbCredentials:{
-    connectionString:env.DB_URL
+  dbCredentials: {
+    connectionString: process.env.DB_URL!,
   },
   out: "./drizzle",
 } satisfies Config;
