66bdb05d80882657fa7985a2f49ef5fcce56101f
Augustin <48162609+AugustinSorel@users.noreply.github.com>
2023-08-12 16:14:30 +0200

    Feature/drizzle next auth adapter (#79)
    * FEATURE/drizzle adapter installed
    
    * FEATURE/User schema added to db
    
    * FEATURE/Sign in with email added
    
    * FIX/type added to import in db schema
    
    * FIX/ts error ignore waiting on next auth for fix
    
    * FIX/next auth secret added
    
    * FIX/middleware updated
    
    * REFACTOR/cleaner authorized callback in middleware
    
    * REFACTOR/authorized callback in middleware removed
    
    * REFACTOR/withAuth removed
    
    * FIX/schema naming changed
    
    * REFACTOR/drizzle adapter updated to 3.1
    
    * REFACTOR/drizzle updated pnpm
    
    * FIX/drizzle config
    
    * FIX/auth core added
    
    * FIX/drizzle folder added
    
    * REFACTOR/drizzle folder cleaned up
    
    * FIX/migration removed

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,10 +1,7 @@
 import { env } from "@/env.mjs";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
-import { migrate } from "drizzle-orm/postgres-js/migrator";
 
 const connectionString = env.DB_URL;
 const sql = postgres(connectionString, { max: 1 });
 export const db = drizzle(sql);
-
-void migrate(db, { migrationsFolder: "drizzle" });
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,16 +3,33 @@ import { getServerSession } from "next-auth";
 import type { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import GitHubProvider from "next-auth/providers/github";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import EmailProvider from "next-auth/providers/email";
+import { db } from "@/db";
 
 export const authOptions: NextAuthOptions = {
+  adapter: DrizzleAdapter(db),
   providers: [
     GoogleProvider({
       clientId: env.GOOGLE_CLIENT_ID,
       clientSecret: env.GOOGLE_CLIENT_SECRET,
+      allowDangerousEmailAccountLinking: true,
     }),
     GitHubProvider({
       clientId: env.GITHUB_CLIENT_ID,
       clientSecret: env.GITHUB_CLIENT_SECRET,
+      allowDangerousEmailAccountLinking: true,
+    }),
+    EmailProvider({
+      server: {
+        host: env.EMAIL_SERVER_HOST,
+        port: env.EMAIL_SERVER_PORT,
+        auth: {
+          user: env.EMAIL_SERVER_USER,
+          pass: env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: env.EMAIL_FROM,
     }),
   ],
   secret: env.NEXTAUTH_SECRET,
