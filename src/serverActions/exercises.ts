86b4c38d56a8763c65feb9415bafdbe49db252b0
AugustinSorel <sorelaugustin@gmail.com>
2023-08-14 11:14:01 +0200

    FEAT/save new exercise to db server action

--- src/serverActions/exercises.ts
+++ src/serverActions/exercises.ts
@@ -1,11 +1,12 @@
 "use server";
 
+import { db } from "@/db";
+import { exercise } from "@/db/schema";
 import type {
   DeleteExerciseSchema,
   UpdateExerciseNameSchema,
   NewExerciseNameSchema,
 } from "@/schemas/exerciseSchemas";
-import { addNewExercise } from "@/fakeData";
 
 export const updateExerciseNameAction = async (e: UpdateExerciseNameSchema) => {
   await new Promise((res) => setTimeout(res, 1_000));
@@ -22,7 +23,8 @@ export const deleteExerciseAction = async (e: DeleteExerciseSchema) => {
 export const addNewExerciseAction = async (
   newExercise: NewExerciseNameSchema
 ) => {
-  await new Promise((res) => setTimeout(res, 1_000));
-
-  addNewExercise({ name: newExercise.name, gridIndex: 0 });
+  return await db
+    .insert(exercise)
+    .values({ ...newExercise })
+    .returning();
 };
