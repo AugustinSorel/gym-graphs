fad464e0ac50c65ec217ffaeb4cbc5099fc65be7
AugustinSorel <sorelaugustin@gmail.com>
2023-09-01 22:29:34 +0200

    REFACTOR/InferSelectModel used over InferModel in db schema

--- src/db/types.ts
+++ src/db/types.ts
@@ -1,6 +1,6 @@
-import type { InferModel } from "drizzle-orm";
+import type { InferSelectModel } from "drizzle-orm";
 import type { exercises, exercisesData, users } from "./schema";
 
-export type Exercise = InferModel<typeof exercises>;
-export type ExerciseData = InferModel<typeof exercisesData>;
-export type User = InferModel<typeof users>;
+export type Exercise = InferSelectModel<typeof exercises>;
+export type ExerciseData = InferSelectModel<typeof exercisesData>;
+export type User = InferSelectModel<typeof users>;
