60896125e0c0ddaa5a923c2f7025a62ddcd54724
AugustinSorel <sorelaugustin@gmail.com>
2023-09-10 14:50:10 +0100

    FEAT/no item in dashboard ui

--- src/app/dashboard/@allExercisesGrid/page.tsx
+++ src/app/dashboard/@allExercisesGrid/page.tsx
@@ -9,6 +9,7 @@ import { redirect } from "next/navigation";
 import { GridLayout } from "../_grid/gridLayout";
 import { TimelineContainer } from "../timelineContainer";
 import { Badge } from "@/components/ui/badge";
+import type { ComponentProps } from "react";
 //TODO: optimistic update when adding / updating / removing exercise
 
 const AllExercisesGrid = async () => {
@@ -27,6 +28,19 @@ const AllExercisesGrid = async () => {
       },
     })
   ).sort((a, b) => b.position.gridPosition - a.position.gridPosition);
+
+  if (exercises.length < 1) {
+    return (
+      <Container>
+        <Title>no data</Title>
+        <Text>
+          Your dashboard is empty
+          <br />
+          Start adding new data with the form above
+        </Text>
+      </Container>
+    );
+  }
 
   return (
     <TimelineContainer>
@@ -76,3 +90,20 @@ const AllExercisesGrid = async () => {
 };
 
 export default AllExercisesGrid;
+
+const Container = (props: ComponentProps<"div">) => {
+  return (
+    <div
+      {...props}
+      className="mt-10 flex flex-col items-center space-y-5 text-center"
+    />
+  );
+};
+
+const Title = (props: ComponentProps<"h1">) => {
+  return <h1 {...props} className="text-6xl font-bold uppercase" />;
+};
+
+const Text = (props: ComponentProps<"p">) => {
+  return <p {...props} className="text-xl text-muted-foreground" />;
+};
