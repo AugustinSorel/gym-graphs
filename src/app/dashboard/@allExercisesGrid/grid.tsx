c79bef9d82254f4fa2019bc918f4245506c576f4
AugustinSorel <sorelaugustin@gmail.com>
2023-10-07 20:50:44 +0100

    REFACTOR/ all exercises grid move to use client

---
+++ src/app/dashboard/@allExercisesGrid/grid.tsx
@@ -0,0 +1,159 @@
+"use client";
+
+import { DragComponent, SortableGrid } from "./sortableGrid";
+import { GridItem } from "../_grid/gridItem";
+import { LineGraph } from "../_graphs/lineGraph";
+import { RadarGraph } from "../_graphs/radarGraph";
+import { GridLayout } from "../_grid/gridLayout";
+import { MoreHorizontal, Tag } from "lucide-react";
+import { useDashboardFilters } from "../dashboardFiltersContext";
+import type { Exercise, ExerciseWithData } from "@/db/types";
+import { MuscleGroupDropdown } from "../muscleGroupsDropdown";
+import {
+  deleteExerciseAction,
+  updateExerciseMuscleGroups,
+  updateExerciseNameAction,
+} from "@/serverActions/exercises";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { UpdateExerciseNameDialog } from "../_modals/updateExerciseNameDialog";
+import { DeleteExerciseAlertDialog } from "../_modals/deleteExerciseAlertDialog";
+import { useState, type PropsWithChildren } from "react";
+
+type Props = {
+  exercises: ExerciseWithData[];
+};
+
+export const Grid = ({ exercises }: Props) => {
+  const dashboardFilters = useDashboardFilters();
+
+  const filteredExercises = dashboardFilters.apply(exercises);
+
+  return (
+    <GridLayout>
+      <SortableGrid
+        gridItems={filteredExercises.map((exercise) => ({
+          id: exercise.id,
+          render: (
+            <GridItem.Root>
+              <GridItem.Anchor
+                aria-label={`go to ${exercise.name}`}
+                href={`/exercises/${exercise.id}`}
+              />
+              <GridItem.Header>
+                <GridItem.Title>{exercise.name}</GridItem.Title>
+
+                <GridItem.ActionContainer>
+                  <ExerciseMuscleGroup exercise={exercise}>
+                    <GridItem.ActionButton aria-label="view exercise tags">
+                      <Tag className="h-4 w-4" />
+                    </GridItem.ActionButton>
+                  </ExerciseMuscleGroup>
+
+                  <ExerciseDropDown exercise={exercise}>
+                    <GridItem.ActionButton aria-label="view more">
+                      <MoreHorizontal className="h-4 w-4" />
+                    </GridItem.ActionButton>
+                  </ExerciseDropDown>
+
+                  <DragComponent id={exercise.id} />
+                </GridItem.ActionContainer>
+              </GridItem.Header>
+
+              <LineGraph data={exercise.data} />
+            </GridItem.Root>
+          ),
+        }))}
+      />
+
+      <GridItem.Root>
+        <GridItem.Header>
+          <GridItem.Title>exercises count</GridItem.Title>
+        </GridItem.Header>
+
+        <RadarGraph
+          data={filteredExercises.map((exercise) => ({
+            exerciseName: exercise.name,
+            frequency: exercise.data.length,
+          }))}
+        />
+      </GridItem.Root>
+    </GridLayout>
+  );
+};
+
+const ExerciseMuscleGroup = ({
+  exercise,
+  children,
+}: { exercise: Exercise } & PropsWithChildren) => {
+  const [selectedMuscleGroups, setSelectedMuscleGroups] = useState(
+    exercise.muscleGroups
+  );
+
+  return (
+    <MuscleGroupDropdown
+      selectedValues={selectedMuscleGroups}
+      updateValues={(newValues) => {
+        setSelectedMuscleGroups(newValues);
+        void updateExerciseMuscleGroups(exercise.id, newValues);
+      }}
+    >
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <DropdownMenuTrigger asChild>{children}</DropdownMenuTrigger>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p className="capitalize">tags</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+    </MuscleGroupDropdown>
+  );
+};
+
+export const ExerciseDropDown = ({
+  exercise,
+  children,
+}: { exercise: Exercise } & PropsWithChildren) => {
+  return (
+    <DropdownMenu>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <DropdownMenuTrigger asChild>{children}</DropdownMenuTrigger>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p className="capitalize">view more</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+      <DropdownMenuContent className="w-56">
+        <DropdownMenuLabel className="capitalize">settings</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <UpdateExerciseNameDialog
+            onAction={updateExerciseNameAction}
+            exercise={exercise}
+          />
+          <DeleteExerciseAlertDialog
+            onAction={deleteExerciseAction}
+            exercise={exercise}
+          />
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
--- src/app/dashboard/@allExercisesGrid/page.tsx
+++ src/app/dashboard/@allExercisesGrid/page.tsx
@@ -1,20 +1,12 @@
-import { DragComponent, SortableGrid } from "./sortableGrid";
-import { GridItem } from "../_grid/gridItem";
-import { LineGraph } from "../_graphs/lineGraph";
-import { RadarGraph } from "../_graphs/radarGraph";
 import { db } from "@/db";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 import { redirect } from "next/navigation";
-import { GridLayout } from "../_grid/gridLayout";
 import { TimelineContainer } from "../timelineContainer";
 import { Badge } from "@/components/ui/badge";
 import type { ComponentProps } from "react";
-import {
-  ExerciseDropDown,
-  ExerciseTagsComboBox,
-} from "../_grid/gridItemActions";
-import { MoreHorizontal, Tag } from "lucide-react";
+import { Grid } from "./grid";
+import type { User } from "@/db/types";
 //TODO: optimistic update when adding / updating / removing exercise
 
 const AllExercisesGrid = async () => {
@@ -24,15 +16,7 @@ const AllExercisesGrid = async () => {
     return redirect("/");
   }
 
-  const exercises = (
-    await db.query.exercises.findMany({
-      where: (exercise, { eq }) => eq(exercise.userId, session.user.id),
-      with: {
-        data: { orderBy: (data, { asc }) => [asc(data.doneAt)] },
-        position: true,
-      },
-    })
-  ).sort((a, b) => b.position.gridPosition - a.position.gridPosition);
+  const exercises = await getExercises(session.user.id);
 
   if (exercises.length < 1) {
     return (
@@ -52,58 +36,8 @@ const AllExercisesGrid = async () => {
       <Badge variant="accent" className="mx-auto lg:ml-0 lg:mr-auto">
         <time dateTime="all">all</time>
       </Badge>
-      <GridLayout>
-        <SortableGrid
-          gridItems={exercises.map((exercise) => ({
-            id: exercise.id,
-            render: (
-              <GridItem.Root>
-                <GridItem.Anchor
-                  aria-label={`go to ${exercise.name}`}
-                  href={`/exercises/${exercise.id}`}
-                />
-                <GridItem.Header>
-                  <GridItem.Title>{exercise.name}</GridItem.Title>
 
-                  <GridItem.ActionContainer>
-                    <ExerciseTagsComboBox
-                      exerciseId={exercise.id}
-                      exerciseMuscleGroups={exercise.muscleGroups}
-                    >
-                      <GridItem.ActionButton aria-label="view exercise tags">
-                        <Tag className="h-4 w-4" />
-                      </GridItem.ActionButton>
-                    </ExerciseTagsComboBox>
-
-                    <ExerciseDropDown exercise={exercise}>
-                      <GridItem.ActionButton aria-label="view more">
-                        <MoreHorizontal className="h-4 w-4" />
-                      </GridItem.ActionButton>
-                    </ExerciseDropDown>
-
-                    <DragComponent id={exercise.id} />
-                  </GridItem.ActionContainer>
-                </GridItem.Header>
-
-                <LineGraph data={exercise.data} />
-              </GridItem.Root>
-            ),
-          }))}
-        />
-
-        <GridItem.Root>
-          <GridItem.Header>
-            <GridItem.Title>exercises count</GridItem.Title>
-          </GridItem.Header>
-
-          <RadarGraph
-            data={exercises.map((exercise) => ({
-              exerciseName: exercise.name,
-              frequency: exercise.data.length,
-            }))}
-          />
-        </GridItem.Root>
-      </GridLayout>
+      <Grid exercises={exercises} />
     </TimelineContainer>
   );
 };
@@ -125,4 +59,16 @@ const Title = (props: ComponentProps<"h1">) => {
 
 const Text = (props: ComponentProps<"p">) => {
   return <p {...props} className="text-xl text-muted-foreground" />;
+};
+
+const getExercises = async (userId: User["id"]) => {
+  return (
+    await db.query.exercises.findMany({
+      where: (exercise, { eq }) => eq(exercise.userId, userId),
+      with: {
+        data: { orderBy: (data, { asc }) => [asc(data.doneAt)] },
+        position: true,
+      },
+    })
+  ).sort((a, b) => b.position.gridPosition - a.position.gridPosition);
 };
