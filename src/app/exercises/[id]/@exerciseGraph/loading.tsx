39c1fc700e0c5d3880a25e46cfd7483833204fcc
AugustinSorel <sorelaugustin@gmail.com>
2023-10-15 10:51:17 +0100

    FEAT/exercise page graph loading page added

---
+++ src/app/exercises/[id]/@exerciseGraph/loading.tsx
@@ -0,0 +1,31 @@
+import type { ComponentProps } from "react";
+
+const Loader = () => {
+  return (
+    <Card>
+      <CardHeader />
+      <CardBody />
+    </Card>
+  );
+};
+
+export default Loader;
+
+const Card = (props: ComponentProps<"div">) => {
+  return (
+    <div
+      {...props}
+      className="border-y border-border bg-primary backdrop-blur-md sm:rounded-md sm:border"
+    />
+  );
+};
+
+const CardHeader = (props: ComponentProps<"header">) => {
+  return (
+    <header {...props} className="h-12 border-b border-border bg-primary p-3" />
+  );
+};
+
+const CardBody = (props: ComponentProps<"div">) => {
+  return <div {...props} className="relative h-[500px] overflow-hidden" />;
+};
