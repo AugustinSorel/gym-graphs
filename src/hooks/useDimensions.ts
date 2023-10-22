840a6806f96f4ef5617e3ed3c4a9afde8b855a13
AugustinSorel <sorelaugustin@gmail.com>
2023-10-22 15:43:16 +0100

    REFACTOR/resizeObserver added

--- src/hooks/useDimensions.ts
+++ src/hooks/useDimensions.ts
@@ -1,6 +1,5 @@
-import { useLayoutEffect, useRef, useState } from "react";
+import { useCallback, useLayoutEffect, useRef, useState } from "react";
 
-//FIXME: use reziseObserver
 //FIXME: remove props
 export const useDimensions = <T extends HTMLElement | SVGElement>(
   DEFAULT_WIDTH: number,
@@ -12,20 +11,28 @@ export const useDimensions = <T extends HTMLElement | SVGElement>(
     height: DEFAULT_HEIGHT,
   });
 
+  const onResizeHandler = useCallback(() => {
+    if (!ref.current) {
+      return;
+    }
+
+    setDimensions({
+      width: ref.current.clientWidth ?? DEFAULT_WIDTH,
+      height: ref.current.clientHeight ?? DEFAULT_HEIGHT,
+    });
+  }, [DEFAULT_HEIGHT, DEFAULT_WIDTH]);
+
   useLayoutEffect(() => {
-    const updateSize = () => {
-      setDimensions({
-        width: ref.current?.clientWidth ?? DEFAULT_WIDTH,
-        height: ref.current?.clientHeight ?? DEFAULT_HEIGHT,
-      });
-    };
+    if (!ref.current) {
+      return;
+    }
 
-    window.addEventListener("resize", updateSize);
+    const observer = new ResizeObserver(onResizeHandler);
 
-    updateSize();
+    observer.observe(ref.current);
 
-    return () => window.removeEventListener("resize", updateSize);
-  }, [DEFAULT_WIDTH, DEFAULT_HEIGHT]);
+    return () => observer.disconnect();
+  }, [onResizeHandler]);
 
   return {
     ref,
