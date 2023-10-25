c3310024554c92d7cbef5c2c691b569518a38b25
AugustinSorel <sorelaugustin@gmail.com>
2023-10-25 18:48:35 +0100

    FEAT/zoom added to the radar component

--- src/app/dashboard/_graphs/radarGraph.tsx
+++ src/app/dashboard/_graphs/radarGraph.tsx
@@ -6,6 +6,8 @@ import { Point } from "@visx/point";
 import { Line, LineRadial } from "@visx/shape";
 import { useDimensions } from "@/hooks/useDimensions";
 import type { Exercise } from "@/db/types";
+import { Zoom } from "@visx/zoom";
+import { Button } from "@/components/ui/button";
 
 export type RadarGraphData = {
   frequency: number;
@@ -36,7 +38,7 @@ const genPoints = (length: number, radius: number) => {
 function genPolygonPoints(
   dataArray: RadarGraphData[],
   scale: (n: number) => number,
-  getValue: (d: RadarGraphData) => number
+  getValue: (d: RadarGraphData) => number,
 ) {
   const step = (Math.PI * 2) / dataArray.length;
   const points = new Array<{ x: number; y: number }>(dataArray.length).fill({
@@ -68,7 +70,7 @@ export type RadarProps = {
 };
 
 export const RadarGraph = ({ data }: { data: RadarGraphData[] }) => {
-  const dimensions = useDimensions<SVGSVGElement>();
+  const dimensions = useDimensions<HTMLDivElement>();
 
   const xMax = dimensions.width - margin.left - margin.right;
   const yMax = dimensions.height - margin.top - margin.bottom;
@@ -92,77 +94,123 @@ export const RadarGraph = ({ data }: { data: RadarGraphData[] }) => {
   const zeroPoint = new Point({ x: 0, y: 0 });
 
   return (
-    <svg
-      height="100%"
-      width="100%"
-      viewBox={`0 0 ${dimensions.width} ${dimensions.height}`}
-      ref={dimensions.ref}
-      className="p-2"
+    <Zoom<SVGSVGElement>
+      width={dimensions.width}
+      height={dimensions.height}
+      key={dimensions.height + dimensions.width}
+      scaleXMin={1 / 2}
+      scaleXMax={4}
+      scaleYMin={1 / 2}
+      scaleYMax={4}
+      initialTransformMatrix={{
+        scaleX: 0.75,
+        scaleY: 0.75,
+        translateX: dimensions.width / 2,
+        translateY: dimensions.height / 2,
+        skewX: 0,
+        skewY: 0,
+      }}
     >
-      <Group top={dimensions.height / 2} left={dimensions.width / 2}>
-        <Group>
-          {[...new Array<number>(levels)].map((_, i) => (
-            <LineRadial
-              key={`web-${i}`}
-              data={webs}
-              angle={(d) => radialScale(d.angle) ?? 0}
-              radius={((i + 1) * radius) / levels}
-              fill="none"
-              className="stroke-muted-foreground"
-              strokeLinecap="round"
-              {...{ suppressHydrationWarning: true }}
-            />
-          ))}
-        </Group>
+      {(zoom) => (
+        <div className="relative h-full overflow-hidden" ref={dimensions.ref}>
+          <svg
+            style={{
+              //TODO:add this to the carousel
+              cursor: zoom.isDragging ? "grabbing" : "grab",
+              touchAction: "none",
+            }}
+            ref={zoom.containerRef}
+            width="100%"
+            height="100%"
+          >
+            <Group transform={zoom.toString()}>
+              <Group>
+                {[...new Array<number>(levels)].map((_, i) => (
+                  <LineRadial
+                    key={`web-${i}`}
+                    data={webs}
+                    angle={(d) => radialScale(d.angle) ?? 0}
+                    radius={((i + 1) * radius) / levels}
+                    fill="none"
+                    className="stroke-muted-foreground"
+                    strokeLinecap="round"
+                    {...{ suppressHydrationWarning: true }}
+                  />
+                ))}
+              </Group>
 
-        <Group>
-          {[...new Array<number>(data.length)].map((_, i) => (
-            <Line
-              key={`radar-line-${i}`}
-              from={zeroPoint}
-              to={points[i]}
-              className="stroke-muted-foreground"
-            />
-          ))}
-        </Group>
+              <Group>
+                {[...new Array<number>(data.length)].map((_, i) => (
+                  <Line
+                    key={`radar-line-${i}`}
+                    from={zeroPoint}
+                    to={points[i]}
+                    className="stroke-muted-foreground"
+                  />
+                ))}
+              </Group>
 
-        <polygon
-          points={polygonPoints.pointString}
-          className="fill-brand-color-two/30 stroke-brand-color-two"
-        />
+              <polygon
+                points={polygonPoints.pointString}
+                className="fill-brand-color-two/30 stroke-brand-color-two"
+              />
 
-        <Group>
-          {data.map((d, i) => {
-            const index = i === 0 ? 1 : i === data.length - 1 ? 0 : i + 1;
-            const point = points[index] ?? { x: 0, y: 0 };
-            return (
-              <text
-                key={`exercise-name-${i}`}
-                y={point.y}
-                x={point.x}
-                className="fill-muted-foreground text-xs"
-                dy={point.y === 0 ? "0.5em" : point.y > 0 ? "1em" : "-0.5em"}
-                dx={point.x === 0 ? "0.5em" : point.x > 0 ? "1em" : "-0.5em"}
-                textAnchor={
-                  point.x === 0 ? "middle" : point.x > 0 ? "start" : "end"
-                }
-              >
-                {d.exerciseName}
-              </text>
-            );
-          })}
-        </Group>
+              <Group>
+                {data.map((d, i) => {
+                  const index = i === 0 ? 1 : i === data.length - 1 ? 0 : i + 1;
+                  const point = points[index] ?? { x: 0, y: 0 };
+                  return (
+                    <text
+                      key={`exercise-name-${i}`}
+                      y={point.y}
+                      x={point.x}
+                      className="select-none fill-muted-foreground text-xs"
+                      dy={
+                        point.y === 0 ? "0.5em" : point.y > 0 ? "1em" : "-0.5em"
+                      }
+                      dx={
+                        point.x === 0 ? "0.5em" : point.x > 0 ? "1em" : "-0.5em"
+                      }
+                      textAnchor={
+                        point.x === 0 ? "middle" : point.x > 0 ? "start" : "end"
+                      }
+                    >
+                      {d.exerciseName}
+                    </text>
+                  );
+                })}
+              </Group>
 
-        {polygonPoints.points.map((point, i) => (
-          <circle
-            key={`radar-point-${i}`}
-            cx={point.x.toFixed(3)}
-            cy={point.y.toFixed(3)}
-            r={4}
-            className="fill-brand-color-two"
-          />
-        ))}
-      </Group>
-    </svg>
+              {polygonPoints.points.map((point, i) => (
+                <circle
+                  key={`radar-point-${i}`}
+                  cx={point.x.toFixed(3)}
+                  cy={point.y.toFixed(3)}
+                  r={4}
+                  className="fill-brand-color-two"
+                />
+              ))}
+            </Group>
+          </svg>
+
+          <div className="absolute bottom-5 right-5 flex gap-2">
+            <Button
+              className="h-8"
+              size="icon"
+              onClick={() => zoom.scale({ scaleX: 1.2, scaleY: 1.2 })}
+            >
+              +
+            </Button>
+            <Button
+              className="h-8"
+              size="icon"
+              onClick={() => zoom.scale({ scaleX: 0.8, scaleY: 0.8 })}
+            >
+              -
+            </Button>
+          </div>
+        </div>
+      )}
+    </Zoom>
   );
 };
