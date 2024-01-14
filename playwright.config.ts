7fc4e758f3dee15ff83e3c5405641fba62195d14
AugustinSorel <sorelaugustin@gmail.com>
2024-01-14 11:59:28 +0000

    FEAT/baseurl env var added to playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,6 +29,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
+    baseURL: process.env.BASE_URL ?? "http://localhost:3000",
   },
 
   /* Configure projects for major browsers */
