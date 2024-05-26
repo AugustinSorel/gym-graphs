5820ec5985f2b33b8358af9df86d22d1cf7fa167
AugustinSorel <sorelaugustin@gmail.com>
2024-05-26 17:09:21 +0100

    CHORE/todos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -462,11 +462,12 @@ const FeaturesGridBackground = () => {
   );
 };
 
-//TODO: test with low end network
 //TODO: error msg
+//TODO: add back loader
+//TODO: fix filtering that is slow
+//TODO: test with low end network
 //TODO: react hook form
 //TODO: add email and psw
 //TODO: move db to server folder
 //TODO: create folder for style
 //TODO: clean the auth componennts
-//TODO: add back loader
