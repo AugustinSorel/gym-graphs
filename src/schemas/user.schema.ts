009031d9b02c52e37e72afcc545c295be0617585
AugustinSorel <sorelaugustin@gmail.com>
2024-06-26 19:34:12 +0100

    FEAT/user schema added

---
+++ src/schemas/user.schema.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+export const userSchema = z.object({
+  id: z
+    .string({
+      required_error: "id is required",
+      invalid_type_error: "id must be a uuid",
+    })
+    .trim()
+    .uuid("uuid is not valid"),
+  email: z
+    .string({ required_error: "email is required" })
+    .email({ message: "email must be valid" })
+    .min(1, { message: "email must be at leat one charater" })
+    .max(255, { message: "email must be at most 255 charaters" }),
+});
