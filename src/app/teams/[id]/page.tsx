118fe2e9d76ea8e1ec4a0c667eff5a1b6824b710
AugustinSorel <sorelaugustin@gmail.com>
2024-06-27 21:08:35 +0100

    FEAT/teams page added

---
+++ src/app/teams/[id]/page.tsx
@@ -0,0 +1,45 @@
+import { getServerAuthSession } from "@/server/auth";
+import { api } from "@/trpc/server";
+import { redirect } from "next/navigation";
+import type { ComponentPropsWithoutRef } from "react";
+import { InviteUserForm } from "./_inviteUserForm/inviteUserForm";
+import {
+  type TeamPageParamsSchema,
+  teamPageParamsSchema,
+} from "./_components/teamPageParams";
+
+type Props = {
+  params: TeamPageParamsSchema;
+};
+
+const Page = async (unsafeProps: Props) => {
+  const params = teamPageParamsSchema.safeParse(unsafeProps.params);
+  const team = await api.team.get({ id: unsafeProps.params.id });
+
+  const session = await getServerAuthSession();
+
+  const userInTeam = team?.usersToTeams.find(
+    (team) => team.memberId === session?.user.id,
+  );
+
+  if (!params.success || !userInTeam) {
+    return redirect("/dashboard");
+  }
+
+  return (
+    <>
+      <FormContainer>
+        <InviteUserForm />
+      </FormContainer>
+
+      <p>here</p>
+      <code>{JSON.stringify(team, undefined, 2)}</code>
+    </>
+  );
+};
+
+export default Page;
+
+const FormContainer = (props: ComponentPropsWithoutRef<"div">) => {
+  return <div {...props} className="p-10" />;
+};
