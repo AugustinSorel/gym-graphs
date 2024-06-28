62bcc794affca48bd0d2f515f9bf1800726d015c
AugustinSorel <sorelaugustin@gmail.com>
2024-06-28 18:55:12 +0100

    FIX/max w in badge

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -3,7 +3,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const badgeVariants = cva(
-  "inline-flex items-center rounded-full border px-4 py-1 text-xs font-bold uppercase transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
+  "inline-block items-center rounded-full border px-4 py-1 text-xs font-bold uppercase max-w-xs truncate transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
   {
     variants: {
       variant: {
@@ -21,7 +21,7 @@ const badgeVariants = cva(
     defaultVariants: {
       variant: "default",
     },
-  }
+  },
 );
 
 export interface BadgeProps
