236ab374145b922c1f0fc1f834262a25fe54a2d3
AugustinSorel <sorelaugustin@gmail.com>
2024-07-13 13:21:01 +0100

    FEAT/rename router

--- src/server/api/routers/user.router.ts
+++ src/server/api/routers/user.router.ts
@@ -1,9 +1,31 @@
 import { users } from "@/server/db/schema";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
 import { eq } from "drizzle-orm";
+import { userSchema } from "@/schemas/user.schema";
+import { TRPCError } from "@trpc/server";
 
 export const userRouter = createTRPCRouter({
+  get: protectedProcedure.query(async ({ ctx }) => {
+    const user = await ctx.db.query.users.findFirst({
+      where: (table, { eq }) => eq(table.id, ctx.session.user.id),
+    });
+
+    if (!user) {
+      throw new TRPCError({ code: "NOT_FOUND", message: "user not found" });
+    }
+
+    return user;
+  }),
   delete: protectedProcedure.mutation(async ({ ctx }) => {
     await ctx.db.delete(users).where(eq(users.id, ctx.session.user.id));
   }),
+
+  rename: protectedProcedure
+    .input(userSchema.pick({ name: true, id: true }))
+    .mutation(async ({ ctx, input }) => {
+      await ctx.db
+        .update(users)
+        .set({ name: input.name })
+        .where(eq(users.id, input.id));
+    }),
 });
