7fd6e853101a97b5cb21e8384b3314f81d7ce14c
AugustinSorel <sorelaugustin@gmail.com>
2024-07-14 14:10:56 +0100

    FIX/adding middleware back

---
+++ src/middleware.ts
@@ -0,0 +1,10 @@
+export { default } from "next-auth/middleware";
+
+export const config = {
+  matcher: [
+    "/dashboard/:path*",
+    "/exercises/:path*",
+    "/teams/:path*",
+    "/account/:path*",
+  ],
+};
