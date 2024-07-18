a8d2192b883ee5ebdd859a6277b6d00eea78d533
AugustinSorel <sorelaugustin@gmail.com>
2024-07-18 21:42:42 +0100

    REFACTOR/new env added in next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
-!process.env.SKIP_ENV_VALIDATION && (await import("./src/env.mjs"));
-
-/** @type {import('next').NextConfig} */
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+await import("./src/env.mjs");
 
 import withPWA from "next-pwa";
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   ...withPWA({
     dest: "public",
