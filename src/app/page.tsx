36fb1f25ee662687f2b619d4294f82cb419fbcc2
Augustin <48162609+AugustinSorel@users.noreply.github.com>
2024-07-19 17:31:17 +0100

    FIX/team random data displays wrong data (#227)
    * FIX/team random data displays wrong data
    
    * CHORE/todo removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -461,7 +461,6 @@ const FeaturesGridBackground = () => {
   );
 };
 
-//TODO: fix random facts in team page show incorrect data
 //TODO: text wrap
 //TODO: fix error in team page
 //TODO: move to type module
