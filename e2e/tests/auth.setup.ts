6016099179acc6153dcd61f0e2dfd49bc5c18b4d
AugustinSorel <sorelaugustin@gmail.com>
2024-07-21 16:32:47 +0100

    FEAT/adding auth test

--- e2e/tests/auth.setup.ts
+++ e2e/tests/auth.setup.ts
@@ -1,34 +1,30 @@
-import { test as setup, expect } from "@playwright/test";
-import dotenv from "dotenv";
-
-dotenv.config();
+import { env } from "@/env";
+import { test as setup } from "@playwright/test";
 
 const authFile = "e2e/.auth/user.json";
 
 const user = {
-  email: process.env.E2E_GOOGLE_USER_EMAIL,
-  password: process.env.E2E_GOOGLE_USER_PASSWORD,
+  email: env.E2E_GITHUB_USER_EMAIL,
+  password: env.E2E_GITHUB_USER_PASSWORD,
 };
 
-setup("authentication", async ({ page }) => {
-  if (!user.email) {
-    throw new Error("user google user email is missing form env");
-  }
+setup("authenticate", async ({ page }) => {
+  await page.goto("/");
 
-  if (!user.password) {
-    throw new Error("user google user password is missing form env");
-  }
+  await page
+    .locator('[href="/dashboard"]', { hasText: "get started" })
+    .first()
+    .click();
 
-  // Perform authentication steps. Replace these actions with your own.
-  await page.goto("https://github.com/login");
+  await page.waitForURL((url) => url.pathname.startsWith("/sign-in"));
+
+  await page.locator("button", { hasText: "github" }).first().click();
+
   await page.getByLabel("Username or email address").fill(user.email);
   await page.getByLabel("Password").fill(user.password);
   await page.getByRole("button", { name: "Sign in" }).click();
 
-  await page.waitForURL("https://github.com/");
-  await expect(
-    page.getByRole("button", { name: "View profile and more" }),
-  ).toBeVisible();
+  await page.waitForURL("/dashboard");
 
   await page.context().storageState({ path: authFile });
 });
