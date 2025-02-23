651b5a9ea65f6f46e5a4d6d0fe30866033c7b1a0
augustinSorel <sorelaugustin@gmail.com>
2025-02-23 18:26:12 +0000

    CHORE/todo added

--- app/routes/__root.tsx
+++ app/routes/__root.tsx
@@ -143,6 +143,7 @@ const RootDocument = (props: Readonly<PropsWithChildren>) => {
 | Type     | Description                                           |
 | ---------| ------------------------------------------------------|
 | TODO     | allow multiple sets                                   |
+| TODO     | gamification                                   |
 | TODO     | invite members                                        |
 | TODO     | ask to join                                           |
 | TODO     | infinite scroll                                       |
