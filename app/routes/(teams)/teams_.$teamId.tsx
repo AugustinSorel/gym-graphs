3100f73b40fc87047f6c7a3da0f0f2a47b849401
augustinSorel <sorelaugustin@gmail.com>
2025-02-24 21:18:32 +0000

    FEAT/basic team page

--- app/routes/(teams)/teams_.$teamId.tsx
+++ app/routes/(teams)/teams_.$teamId.tsx
@@ -4,6 +4,10 @@ import { permissions } from "~/libs/permissions";
 import { teamQueries } from "~/team/team.queries";
 import { teamSchema } from "~/team/team.schemas";
 import { Button } from "~/ui/button";
+import { useTeam } from "~/team/hooks/use-team";
+import { ArrowLeft, Cog } from "lucide-react";
+import { Separator } from "~/ui/separator";
+import type { ComponentProps } from "react";
 
 export const Route = createFileRoute("/(teams)/teams_/$teamId")({
   params: z.object({
@@ -24,18 +28,52 @@ export const Route = createFileRoute("/(teams)/teams_/$teamId")({
 
 const RouteComponent = () => {
   const params = Route.useParams();
+  const team = useTeam(params.teamId);
 
   return (
-    <>
-      <div>Hello "/(teams)/teams/$teamId"!</div>
-      <Button asChild variant="outline">
-        <Link to="/teams/$teamId/settings" params={{ teamId: params.teamId }}>
-          settings
-        </Link>
-      </Button>
-      <Button asChild>
-        <Link to="..">back</Link>
-      </Button>
-    </>
+    <Main>
+      <Header>
+        <Title>{team.data.name}</Title>
+        <Button variant="outline" size="sm" asChild>
+          <Link
+            to="/teams/$teamId/settings"
+            from={Route.fullPath}
+            aria-label="team settings"
+          >
+            <span className="hidden lg:inline-flex">settings</span>
+            <Cog className="lg:hidden" />
+          </Link>
+        </Button>
+        <Button
+          asChild
+          variant="link"
+          className="text-muted-foreground mr-auto p-0"
+        >
+          <Link to="..">
+            <ArrowLeft />
+            <span>back</span>
+          </Link>
+        </Button>
+      </Header>
+
+      <Separator />
+    </Main>
   );
+};
+
+const Main = (props: ComponentProps<"main">) => {
+  return (
+    <main
+      className="max-w-app mx-auto flex flex-col gap-10 px-2 pt-10 pb-20 lg:gap-20 lg:px-4 lg:pt-20"
+      {...props}
+    />
+  );
+};
+
+const Header = (props: ComponentProps<"header">) => {
+  return <header className="grid grid-cols-[1fr_auto] gap-2" {...props} />;
+};
+
+const Title = (props: ComponentProps<"h1">) => {
+  return <h1 className="text-3xl font-semibold capitalize" {...props} />;
 };
