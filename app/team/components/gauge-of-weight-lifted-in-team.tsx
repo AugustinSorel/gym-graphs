17c545160311b05787c868cb2f49ff1cc2800318
augustinSorel <sorelaugustin@gmail.com>
2025-03-03 12:56:04 +0000

    REFACTOR/better tooltip in gauge

--- app/team/components/gauge-of-weight-lifted-in-team.tsx
+++ app/team/components/gauge-of-weight-lifted-in-team.tsx
@@ -1,6 +1,6 @@
 import { Group } from "@visx/group";
 import { ParentSize } from "@visx/responsive";
-import { Arc } from "@visx/shape";
+import { Arc, Bar } from "@visx/shape";
 import { Text } from "@visx/text";
 import { convertUserWeightUnitToSymbol } from "~/weight-unit/weight-unit.utils";
 import { useUser } from "~/user/hooks/use-user";
@@ -11,8 +11,14 @@ import { WeightUnit } from "~/weight-unit/components/weight-unit";
 import { cn } from "~/styles/styles.utils";
 import { useTeam } from "~/team/hooks/use-team";
 import { getRouteApi } from "@tanstack/react-router";
-import type { ComponentProps, CSSProperties } from "react";
 import type { Set } from "~/db/db.schemas";
+import {
+  type MouseEvent,
+  type TouchEvent,
+  type ComponentProps,
+  type CSSProperties,
+  useCallback,
+} from "react";
 
 export const GaugeOfWeightLiftedInTeam = () => {
   const weightLiftedInTeam = useWeightLiftedInTeam();
@@ -46,11 +52,77 @@ const Graph = (props: GraphProps) => {
 
   const radius = 120;
   const angleRange = Math.PI;
-  let tooltipTimeout: number;
 
   const normalizedValue = Math.max(
     0,
     Math.min(1, (props.userTotalWeightInKg - min) / (max - min)),
+  );
+
+  const handleTooltip = useCallback(
+    (event: TouchEvent<SVGRectElement> | MouseEvent<SVGRectElement>) => {
+      const point = localPoint(event);
+
+      if (!point) {
+        return;
+      }
+      const data = [
+        props.userTotalWeightInKg,
+        props.totalWeightInKg - props.userTotalWeightInKg,
+      ] as const;
+
+      const cx = props.width / 2;
+      const cy = props.height / 2 + 55;
+
+      const x = point.x - cx;
+      const y = point.y - cy;
+
+      let angle = Math.atan2(x, y);
+      if (angle < 0) {
+        angle += 2 * Math.PI;
+      }
+
+      const offset =
+        (360 * props.userTotalWeightInKg) / props.totalWeightInKg / 2;
+
+      const angleDegrees = (angle * 180) / Math.PI;
+      const z = Math.abs(((angleDegrees + 90 - offset) % 360) - 360);
+
+      const sum = props.totalWeightInKg;
+
+      const angles = data.map((arc) => {
+        return (360 * arc) / sum;
+      });
+
+      let index = 0;
+      let accum = 0;
+
+      for (const angle of angles) {
+        accum += angle;
+
+        if (z < accum) {
+          break;
+        }
+
+        ++index;
+      }
+
+      const tooltipAngle = accum - (angles.at(index) ?? 0) * 0.5;
+
+      const tooltipAngleRadians = (tooltipAngle * Math.PI) / 180;
+
+      const tooltipX = cx - radius * Math.sin(tooltipAngleRadians) * 4;
+      const tooltipY = 0;
+
+      tooltip.showTooltip({
+        tooltipLeft: Math.max(tooltipX, 0),
+        tooltipTop: Math.max(tooltipY, 0),
+        tooltipData: {
+          entity: index === 0 ? "you" : "team",
+          weightInKg: data.at(index) ?? 0,
+        },
+      });
+    },
+    [tooltip, props],
   );
 
   return (
@@ -59,65 +131,49 @@ const Graph = (props: GraphProps) => {
         <Group top={props.height / 2 + margin.top} left={props.width / 2}>
           <Arc
             suppressHydrationWarning
-            innerRadius={radius - 10}
+            innerRadius={radius - 15}
             outerRadius={radius}
             startAngle={-Math.PI / 2}
-            endAngle={-Math.PI / 2 + normalizedValue * angleRange}
+            endAngle={-Math.PI / 2 + normalizedValue * angleRange - 0.05}
             cornerRadius={6}
-            className="fill-primary"
-            onMouseLeave={() => {
-              tooltipTimeout = window.setTimeout(() => {
-                tooltip.hideTooltip();
-              }, 300);
-            }}
-            onMouseMove={(e) => {
-              if (tooltipTimeout) {
-                clearTimeout(tooltipTimeout);
-              }
-
-              const eventSvgCoords = localPoint(e);
-
-              tooltip.showTooltip({
-                tooltipData: {
-                  entity: "user",
-                  weightInKg: props.userTotalWeightInKg,
-                },
-                tooltipTop: eventSvgCoords?.y,
-                tooltipLeft: eventSvgCoords?.x,
-              });
-            }}
+            className="fill-primary/80"
           />
 
           <Arc
             suppressHydrationWarning
-            innerRadius={radius - 10}
+            innerRadius={radius - 15}
             outerRadius={radius}
             startAngle={Math.PI / 2}
             endAngle={-Math.PI / 2 + normalizedValue * angleRange}
             cornerRadius={6}
-            className="fill-border"
-            onMouseLeave={() => {
-              tooltipTimeout = window.setTimeout(() => {
-                tooltip.hideTooltip();
-              }, 300);
-            }}
-            onMouseMove={(e) => {
-              if (tooltipTimeout) {
-                clearTimeout(tooltipTimeout);
-              }
-
-              const eventSvgCoords = localPoint(e);
-
-              tooltip.showTooltip({
-                tooltipData: {
-                  entity: "team",
-                  weightInKg: props.totalWeightInKg - props.userTotalWeightInKg,
-                },
-                tooltipTop: eventSvgCoords?.y,
-                tooltipLeft: eventSvgCoords?.x,
-              });
-            }}
+            className="fill-border/80"
           />
+
+          {/*team tooltip indicator*/}
+          {tooltip.tooltipData && tooltip.tooltipData.entity === "team" && (
+            <Arc
+              suppressHydrationWarning
+              innerRadius={radius - 15}
+              outerRadius={radius}
+              startAngle={Math.PI / 2}
+              endAngle={-Math.PI / 2 + normalizedValue * angleRange}
+              cornerRadius={6}
+              className="fill-border"
+            />
+          )}
+
+          {/*user tooltip indicator*/}
+          {tooltip.tooltipData && tooltip.tooltipData.entity === "you" && (
+            <Arc
+              suppressHydrationWarning
+              innerRadius={radius - 15}
+              outerRadius={radius}
+              startAngle={-Math.PI / 2}
+              endAngle={-Math.PI / 2 + normalizedValue * angleRange - 0.05}
+              cornerRadius={6}
+              className="fill-primary"
+            />
+          )}
 
           <Text
             textAnchor="middle"
@@ -129,12 +185,25 @@ const Graph = (props: GraphProps) => {
             {`${props.userTotalWeightInKg} ${convertUserWeightUnitToSymbol(user.data.weightUnit)}`}
           </Text>
         </Group>
+
+        {/*hit zone for tooltip*/}
+        <Group top={0} left={0}>
+          <Bar
+            width={props.width}
+            height={props.height}
+            fill="transparent"
+            onMouseMove={handleTooltip}
+            onTouchStart={handleTooltip}
+            onTouchMove={handleTooltip}
+            onMouseLeave={tooltip.hideTooltip}
+          />
+        </Group>
       </svg>
 
       {tooltip.tooltipData && (
         <Tooltip
           top={Math.min(tooltip.tooltipTop ?? 0, props.height - 75)}
-          left={Math.min(tooltip.tooltipLeft ?? 0, props.width - 130)}
+          left={Math.min(tooltip.tooltipLeft ?? 0, props.width - 175)}
           style={tooltipStyles}
         >
           <p className="text-xs font-bold capitalize">
@@ -144,7 +213,7 @@ const Graph = (props: GraphProps) => {
             <dt
               className={cn(
                 "text-muted-foreground flex items-center before:mr-2 before:block before:size-2",
-                tooltip.tooltipData.entity === "user" && "before:bg-primary",
+                tooltip.tooltipData.entity === "you" && "before:bg-primary",
                 tooltip.tooltipData.entity === "team" &&
                   "before:bg-muted-foreground",
               )}
@@ -193,7 +262,7 @@ type GraphProps = Readonly<{
   userTotalWeightInKg: Set["weightInKg"];
 }>;
 
-type TooltipData = { weightInKg: Set["weightInKg"]; entity: "user" | "team" };
+type TooltipData = { weightInKg: Set["weightInKg"]; entity: "you" | "team" };
 
 const tooltipStyles: Readonly<CSSProperties> = {
   ...defaultStyles,
