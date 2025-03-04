5fc74a51b1d5b38ad4e1a60ce9129899df88f5a1
augustinSorel <sorelaugustin@gmail.com>
2025-03-04 10:59:44 +0000

    FIX/alert icon

--- app/ui/icons.tsx
+++ app/ui/icons.tsx
@@ -39,8 +39,10 @@ export const ArrowRightIcon = (props: ComponentProps<typeof Icon>) => {
 
 export const AlertCircleIcon = (props: ComponentProps<typeof Icon>) => {
   return (
-    <Icon {...props}>
-      <path d="m12.002 21.534c5.518 0 9.998-4.48 9.998-9.998s-4.48-9.997-9.998-9.997c-5.517 0-9.997 4.479-9.997 9.997s4.48 9.998 9.997 9.998zm0-1.5c-4.69 0-8.497-3.808-8.497-8.498s3.807-8.497 8.497-8.497 8.498 3.807 8.498 8.497-3.808 8.498-8.498 8.498zm0-6.5c-.414 0-.75-.336-.75-.75v-5.5c0-.414.336-.75.75-.75s.75.336.75.75v5.5c0 .414-.336.75-.75.75zm-.002 3c.552 0 1-.448 1-1s-.448-1-1-1-1 .448-1 1 .448 1 1 1z" />
+    <Icon {...props} strokeLinecap="round" strokeLinejoin="round">
+      <circle cx="12" cy="12" r="10" />
+      <line x1="12" x2="12" y1="8" y2="12" />
+      <line x1="12" x2="12.01" y1="16" y2="16" />
     </Icon>
   );
 };
