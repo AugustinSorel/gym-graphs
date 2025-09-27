d15e19249c32fcfd5b873c54b15e13a38333aeaa
augustinSorel <sorelaugustin@gmail.com>
2025-09-27 12:21:26 +0200

    CHORE/dev tool updated

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -20,7 +20,7 @@ const TanStackRouterDevtools =
   process.env.NODE_ENV === "production"
     ? () => null
     : lazy(() =>
-        import("@tanstack/router-devtools").then((res) => ({
+        import("@tanstack/react-router-devtools").then((res) => ({
           default: res.TanStackRouterDevtools,
         })),
       );
