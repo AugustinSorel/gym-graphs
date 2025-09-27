b8d47223d222a1a5a6d60c3e8b8194ddfc591003
augustinSorel <sorelaugustin@gmail.com>
2025-09-27 12:24:55 +0200

    FIX/middleware back

--- src/start.tsx
+++ src/start.tsx
@@ -1,7 +1,9 @@
 import { createStart } from "@tanstack/react-start";
+import { errorMiddleware } from "~/libs/error";
+import { rateLimiterMiddleware } from "~/auth/auth.middlewares";
 
 export const startInstance = createStart(() => {
   return {
-    requestMiddleware: [],
+    requestMiddleware: [errorMiddleware, rateLimiterMiddleware],
   };
 });
