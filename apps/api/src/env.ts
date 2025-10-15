8dd992feba64959773279241c939967d43e1e533
augustinSorel <sorelaugustin@gmail.com>
2025-10-15 10:29:28 +0200

    FIX/process.env

--- apps/api/src/env.ts
+++ apps/api/src/env.ts
@@ -28,27 +28,4 @@ const envSchema = z.object({
   GITHUB_CLIENT_SECRET: z.string().trim().nonempty(),
 });
 
-export const env = envSchema.parse({
-  NODE_ENV: process.env.NODE_ENV,
-  PORT: process.env.PORT,
-
-  DB_PASSWORD: process.env.DB_PASSWORD,
-  DB_USER: process.env.DB_USER,
-  DB_NAME: process.env.DB_NAME,
-  DB_HOST: process.env.DB_HOST,
-  DB_PORT: process.env.DB_PORT,
-
-  CACHE_PASSWORD: process.env.CACHE_PASSWORD,
-  CACHE_USER: process.env.CACHE_USER,
-  CACHE_HOST: process.env.CACHE_HOST,
-  CACHE_NAME: process.env.CACHE_NAME,
-  CACHE_PORT: process.env.CACHE_PORT,
-
-  SMTP_HOST: process.env.SMTP_HOST,
-  SMTP_USER: process.env.SMTP_USER,
-  SMTP_PASSWORD: process.env.SMTP_PASSWORD,
-  SMTP_FROM: process.env.SMTP_FROM,
-
-  GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
-  GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
-});
+export const env = envSchema.parse(process.env);
