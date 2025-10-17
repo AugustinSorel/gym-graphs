61dbed5f252ca31ab1c4c0741d10c5c78e61c833
augustinSorel <sorelaugustin@gmail.com>
2025-10-17 11:29:19 +0200

    REFACTOR/parseJSONResponse

--- apps/web/src/libs/api.ts
+++ apps/web/src/libs/api.ts
@@ -1,4 +1,4 @@
-import { hc } from "hono/client";
+import { DetailedError, hc, parseResponse } from "hono/client";
 import z from "zod";
 import { constant } from "@gym-graphs/constants";
 import type { Api } from "@gym-graphs/api";
@@ -9,38 +9,25 @@ export const api = hc<Api>(constant.url.api, {
 }).api;
 
 export const parseJsonResponse = async <T extends ClientResponse<any>>(
-  fetchRes: T | Promise<T>,
+  req: T | Promise<T>,
 ) => {
-  const res = await fetchRes;
+  try {
+    return await parseResponse(req);
+  } catch (e) {
+    if (e instanceof DetailedError) {
+      const msg = detailsErrorSchema.catch(e.message).parse(e.detail);
 
-  const text = await res.text();
+      throw new Error(msg, { cause: e });
+    }
 
-  if (!res.ok) {
-    const errorData = jsonMessageStringSchema.parse(text);
-
-    throw new Error(errorData.message);
+    throw e;
   }
-
-  if (!text) {
-    return null;
-  }
-
-  return JSON.parse(text) as T extends ClientResponse<infer U> ? U : never;
 };
 
-const jsonMessageStringSchema = z
-  .preprocess(
-    (raw) => {
-      if (typeof raw !== "string") {
-        return raw;
-      }
-
-      try {
-        return JSON.parse(raw);
-      } catch {
-        return raw;
-      }
-    },
-    z.object({ message: z.string() }),
-  )
-  .catch({ message: "Unknown error" });
+const detailsErrorSchema = z
+  .object({
+    data: z.object({
+      message: z.string(),
+    }),
+  })
+  .transform((err) => err.data.message);
