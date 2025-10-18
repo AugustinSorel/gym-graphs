59dc85bdd9af26291d1e347ced3c21497cfae820
augustinSorel <sorelaugustin@gmail.com>
2025-10-18 09:52:30 +0200

    FEAT/auth check in dashboard page

--- apps/web/src/routes/dashboard.tsx
+++ apps/web/src/routes/dashboard.tsx
@@ -1,9 +1,14 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, redirect } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/dashboard')({
+export const Route = createFileRoute("/dashboard")({
   component: RouteComponent,
-})
+  beforeLoad: ({ context }) => {
+    if (!context.user?.emailVerifiedAt) {
+      throw redirect({ to: "/sign-in" });
+    }
+  },
+});
 
 function RouteComponent() {
-  return <div>Hello "/dashboard"!</div>
+  return <div>Hello "/dashboard"!</div>;
 }
