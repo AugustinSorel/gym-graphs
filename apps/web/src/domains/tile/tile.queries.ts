19820145cfb922918739887d4d4981088e50184f
augustinSorel <sorelaugustin@gmail.com>
2025-10-28 17:51:20 +0100

    REFACTOR/tile

--- apps/web/src/domains/tile/tile.queries.ts
+++ apps/web/src/domains/tile/tile.queries.ts
@@ -1,23 +1,27 @@
 import { infiniteQueryOptions } from "@tanstack/react-query";
 import { api, parseJsonResponse } from "~/libs/api";
 import type { Tile, Tag } from "@gym-graphs/api";
+import { InferRequestType } from "hono";
 
 const all = (name?: Tile["name"], tags?: Array<Tag["name"]>) => {
   return infiniteQueryOptions({
     queryKey: ["dashboard", "tiles", name, tags],
     queryFn: async ({ pageParam, signal }) => {
-      const req = api().tiles.$get(
-        {
-          query: {
-            page: pageParam.toString(),
-            name: name ?? "",
-            tags: JSON.stringify(tags),
-          },
-        },
-        { init: { signal } },
-      );
+      const req = api().tiles.$get;
 
-      return parseJsonResponse(req);
+      const query: InferRequestType<typeof req>["query"] = {
+        page: pageParam.toString(),
+      };
+
+      if (name) {
+        query.name = name;
+      }
+
+      if (tags?.length) {
+        query.tags = JSON.stringify(tags);
+      }
+
+      return parseJsonResponse(req({ query }, { init: { signal } }));
     },
     initialPageParam: 1,
     getNextPageParam: (lastPage) => lastPage.nextCursor,
