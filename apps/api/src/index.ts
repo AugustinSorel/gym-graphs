299d19b1fbb5a15a84a6c764e366d3e65d8a5f8e
augustinSorel <sorelaugustin@gmail.com>
2026-03-12 11:01:54 +0100

    FEAT/loggers

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -3,30 +3,32 @@ import {
   HttpApiBuilder,
   HttpApiEndpoint,
   HttpApiGroup,
+  HttpServer,
 } from "@effect/platform";
 import { NodeHttpServer, NodeRuntime } from "@effect/platform-node";
 import { Effect, Layer, pipe, Schema } from "effect";
 import { createServer } from "node:http";
 import { serverConfig } from "./env";
 
-const MyApi = HttpApi.make("MyApi").add(
+const Api = HttpApi.make("Api").add(
   HttpApiGroup.make("Greetings").add(
     HttpApiEndpoint.get("hello-world")`/`.addSuccess(Schema.String),
   ),
 );
 
-const GreetingsLive = HttpApiBuilder.group(MyApi, "Greetings", (handlers) =>
+const GreetingsLive = HttpApiBuilder.group(Api, "Greetings", (handlers) =>
   handlers.handle("hello-world", () => Effect.succeed("Hello, World!")),
 );
 
-const MyApiLive = HttpApiBuilder.api(MyApi).pipe(Layer.provide(GreetingsLive));
+const ApiLive = HttpApiBuilder.api(Api).pipe(Layer.provide(GreetingsLive));
 
 const ServerLive = Layer.unwrapEffect(
   pipe(
     serverConfig,
     Effect.andThen((env) =>
       HttpApiBuilder.serve().pipe(
-        Layer.provide(MyApiLive),
+        HttpServer.withLogAddress,
+        Layer.provide(ApiLive),
         Layer.provide(NodeHttpServer.layer(createServer, { port: env.port })),
       ),
     ),
