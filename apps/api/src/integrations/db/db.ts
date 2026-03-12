c49ef0f59c59a890282e785fb9fd65638fe3ab58
augustinSorel <sorelaugustin@gmail.com>
2026-03-12 16:58:22 +0100

    FEAT/db

--- apps/api/src/integrations/db/db.ts
+++ apps/api/src/integrations/db/db.ts
@@ -1,7 +1,33 @@
-import { serverConfig } from "#/env";
-import { drizzle } from "drizzle-orm/node-postgres";
-import { Effect, Redacted } from "effect";
+import { Effect, Layer } from "effect";
+import { PgClient } from "@effect/sql-pg";
+import { types } from "pg";
+import * as PgDrizzle from "drizzle-orm/effect-postgres";
+import { ServerConfig } from "#/env";
 
-const env = Effect.runSync(serverConfig);
+export const PgClientLive = Layer.unwrapEffect(
+  Effect.gen(function* () {
+    const env = yield* ServerConfig;
 
-const db = drizzle(Redacted.value(env.db.url));
+    return PgClient.layer({
+      url: env.db.url,
+      types: {
+        getTypeParser: (typeId, format) => {
+          if (
+            // Return raw values for date/time types to let Drizzle handle parsing
+            [1184, 1114, 1082, 1186, 1231, 1115, 1185, 1187, 1182].includes(
+              typeId,
+            )
+          ) {
+            return (val: any) => val;
+          }
+          return types.getTypeParser(typeId, format);
+        },
+      },
+    });
+  }),
+);
+
+export class Database extends Effect.Service<Database>()("Database", {
+  effect: PgDrizzle.make().pipe(Effect.provide(PgDrizzle.DefaultServices)),
+  accessors: true,
+}) {}
--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,50 +1,11 @@
-import { Context, Effect, Layer } from "effect";
+import { Effect, Layer } from "effect";
 import { ServerConfig } from "./env";
 import { HttpApiBuilder, HttpApiSwagger, HttpServer } from "@effect/platform";
 import { NodeHttpServer } from "@effect/platform-node";
 import { createServer } from "node:http";
 import { AuthLive } from "#/features/auth/handlers";
 import { Api } from "#/api";
-import { PgClient } from "@effect/sql-pg";
-import { types } from "pg";
-import * as PgDrizzle from "drizzle-orm/effect-postgres";
-
-const PgClientLive = Layer.unwrapEffect(
-  Effect.gen(function* () {
-    const env = yield* ServerConfig;
-
-    return PgClient.layer({
-      url: env.db.url,
-      types: {
-        getTypeParser: (typeId, format) => {
-          if (
-            [1184, 1114, 1082, 1186, 1231, 1115, 1185, 1187, 1182].includes(
-              typeId,
-            )
-          ) {
-            return (val: any) => val;
-          }
-          return types.getTypeParser(typeId, format);
-        },
-      },
-    });
-  }),
-);
-const dbEffect = PgDrizzle.make().pipe(
-  Effect.provide(PgDrizzle.DefaultServices),
-);
-
-class DB extends Context.Tag("DB")<
-  DB,
-  Effect.Effect.Success<typeof dbEffect>
->() {}
-
-const DBLive = Layer.effect(
-  DB,
-  Effect.gen(function* () {
-    return yield* dbEffect;
-  }),
-);
+import { Database, PgClientLive } from "./integrations/db/db";
 
 const ApiLive = HttpApiBuilder.api(Api).pipe(Layer.provide(AuthLive));
 
@@ -52,7 +13,7 @@ export const ServerLive = HttpApiBuilder.serve().pipe(
   HttpServer.withLogAddress,
   Layer.provide(HttpApiSwagger.layer({ path: "/doc" })),
   Layer.provide(ApiLive),
-  Layer.provide(Layer.provideMerge(DBLive, PgClientLive)),
+  Layer.provide(Layer.provideMerge(Database.Default, PgClientLive)),
   Layer.provide(
     Layer.unwrapEffect(
       Effect.gen(function* () {
