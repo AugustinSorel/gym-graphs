eabae0e02f19842be74497a6aa6c92dc307c5bd2
augustinSorel <sorelaugustin@gmail.com>
2026-06-17 23:17:53 +0200

    FEAT/project nuked

--- apps/api/drizzle/20260423165217_woozy_thunderbolts/migration.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "users" DROP COLUMN "dashboardView";--> statement-breakpoint
-DROP TYPE "dashboard_view";
--- apps/web/src/domains/user/components/weight-unit.tsx
+++
@@ -1,9 +0,0 @@
-import { useSuspenseQuery } from "@tanstack/react-query";
-import { userQueries } from "~/domains/user/user.queries";
-import { convertWeightUnitToSymbol } from "~/domains/user/user.utils";
-
-export const WeightUnit = () => {
-  const user = useSuspenseQuery(userQueries.get);
-
-  return convertWeightUnitToSymbol(user.data.weightUnit);
-};
