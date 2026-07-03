ba5c28e4856160d3656902b76fc20520a9515ce0
augustinSorel <sorelaugustin@gmail.com>
2026-07-03 22:17:50 +0200

    FEAT/one rep max migrations

---
+++ migrations/20260703000001_one_rep_max_algorithm.sql
@@ -0,0 +1,23 @@
+-- +goose Up
+create type one_rep_max_algorithm as enum (
+    'adams',
+    'baechle',
+    'berger',
+    'brown',
+    'brzycki',
+    'epley',
+    'kemmler',
+    'landers',
+    'lombardi',
+    'mayhew',
+    'naclerio',
+    'oconner',
+    'wathen'
+);
+
+alter table users
+    add column one_rep_max_algorithm one_rep_max_algorithm not null default 'epley';
+
+-- +goose Down
+alter table users drop column one_rep_max_algorithm;
+drop type one_rep_max_algorithm;
