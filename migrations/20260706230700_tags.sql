441dd4535499d1f42cdb70f659c28192146d1bfc
augustinSorel <sorelaugustin@gmail.com>
2026-07-07 01:18:58 +0200

    FEAT/tags migration

---
+++ migrations/20260706230700_tags.sql
@@ -0,0 +1,12 @@
+-- +goose Up
+create table tags (
+    id integer generated always as identity primary key not null,
+    user_id integer references users(id) on delete cascade not null,
+    name text not null,
+    updated_at timestamp default now() not null,
+    created_at timestamp default now() not null,
+    unique (user_id, name)
+);
+
+-- +goose Down
+drop table if exists tags;
