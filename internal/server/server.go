627307e8f4b29db2438f20a5c97d7ad2b5fa3fbb
augustinSorel <sorelaugustin@gmail.com>
2026-07-21 01:18:08 +0200

    FEAT/hello world

---
+++ internal/server/server.go
@@ -0,0 +1,42 @@
+package server
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+	// _ "github.com/joho/godotenv/autoload"
+	// "hello/internal/database"
+)
+
+type Server struct {
+	port int
+
+	// db database.Service
+}
+
+func NewServer() *http.Server {
+	port, err := strconv.Atoi(os.Getenv("PORT"))
+
+	if err != nil {
+		panic(err)
+	}
+
+	NewServer := &Server{
+		port: port,
+
+		// db: database.New(),
+	}
+
+	// Declare Server config
+	server := &http.Server{
+		Addr:         fmt.Sprintf(":%d", NewServer.port),
+		Handler:      NewServer.RegisterRoutes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	return server
+}
