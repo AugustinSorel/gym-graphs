377db9ea4c351eef9eb2aee6c7619e992beaf870
augustinSorel <sorelaugustin@gmail.com>
2026-07-22 22:25:28 +0200

    FIX/goose migration

--- devenv.nix
+++ devenv.nix
@@ -6,7 +6,7 @@
 
     GOOSE_DRIVER = "postgres";
     GOOSE_DBSTRING = "postgres://localhost:5432/gym_graphs";
-    GOOSE_MIGRATION_DIR = ./migrations;
+    GOOSE_MIGRATION_DIR = ./internal/db/migrations;
 
     DB_HOST = "localhost";
     DB_PORT = "5432";
