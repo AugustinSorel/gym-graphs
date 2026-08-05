2e626e2c39b626b766556ee9fdf7da4c29687d3e
augustinSorel <sorelaugustin@gmail.com>
2026-08-06 00:59:36 +0200

    FIX/algos

--- internal/onerepmax/onerepmax.go
+++ internal/onerepmax/onerepmax.go
@@ -29,7 +29,7 @@ type formulaFn func(w, r float64) float64
 var formulas = map[string]formulaFn{
 	"adams":    func(w, r float64) float64 { return w / (1 - 0.02*r) },
 	"baechle":  func(w, r float64) float64 { return w * (1 + 0.033*r) },
-	"berger":   func(w, r float64) float64 { return w / (1.0261 * math.Exp(-0.0262*r)) },
+	"berger":   func(w, r float64) float64 { return w / math.Pow(1.0261, 0.0262*r) },
 	"brown":    func(w, r float64) float64 { return w * (0.9849 + 0.0328*r) },
 	"brzycki":  func(w, r float64) float64 { return w * (36 / (37 - r)) },
 	"epley":    func(w, r float64) float64 { return w * (1 + r/30) },
@@ -37,7 +37,7 @@ var formulas = map[string]formulaFn{
 	"landers":  func(w, r float64) float64 { return w / (1.013 - 0.0267123*r) },
 	"lombardi": func(w, r float64) float64 { return w * math.Pow(r, 0.1) },
 	"mayhew":   func(w, r float64) float64 { return (100 * w) / (52.2 + 41.9*math.Exp(-0.055*r)) },
-	"naclerio": func(w, r float64) float64 { return w * (0.951*math.Exp(-0.021*r) + 0.068) },
+	"naclerio": func(w, r float64) float64 { return w / (0.951 * math.Exp(-0.021*r)) },
 	"oconner":  func(w, r float64) float64 { return w * (1 + 0.025*r) },
 	"wathen":   func(w, r float64) float64 { return (100 * w) / (48.8 + 53.8*math.Exp(-0.075*r)) },
 }
